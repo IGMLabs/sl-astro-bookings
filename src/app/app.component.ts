09aed19f18560efac76aef2de05798c0fac77be5
susana <sulopezfeijoo@gmail.com>
2022-06-20 09:24:32 +0200

    pwa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SwUpdate, VersionEvent } from '@angular/service-worker';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+public newVersion = '';
+
+constructor(swUpdate: SwUpdate) {
+  swUpdate.versionUpdates.subscribe((event: VersionEvent) => {
+    if (event.type === 'VERSION_READY') {
+      this.newVersion = event.latestVersion.appData
+        ? JSON.stringify(event.latestVersion.appData)
+        : event.latestVersion.hash;
+    }
+  });
+  swUpdate.checkForUpdate();
+    interval(1000 * 60 * 60).subscribe(() => swUpdate.checkForUpdate());
+}
+
+public onReload() {
+  window.location.reload();
+}
 
 
 }
